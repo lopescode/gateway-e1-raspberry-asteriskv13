76d83893d2d8048ffe6e056a59499b6edb7b6547
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-06-25 15:42:41 -0300

    Melhorando raspInit

--- raspInit.sh
+++ raspInit.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+###############################################
+cd /usr/src/gateway-e1-raspberry-asteriskv13/ #
+###############################################
+
+# Configurando alias no bash
+echo "Configurando alias no bash"
+echo "alias ll='ls-la'" >> ~/.bashrc
+
 # Configurando rede interna
 echo "Configurando rede interna"
 sudo mv ./interfaces /etc/network/interfaces
@@ -18,92 +26,122 @@ sudo mv ./rc.local /etc/rc.local
 
 # Reiniciando o serviço de rede
 echo "Reiniciando o serviço de rede, rotas e ssh"
-/etc/init.d/networking restart
-/etc/init.d/routes
-/etc/init.d/ssh restart
+sudo /etc/init.d/networking restart
+sudo /etc/init.d/routes
+sudo /etc/init.d/ssh restart
 
 # Instalando pacotes do Linux para a instalação do asterisk
 echo "Instalando pacotes do Linux para a instalação do asterisk"
-apt install ssh make vim build-essential git-core subversion libjansson-dev libsqlite3-dev autoconf automake libxml2-dev libncurses5-dev libtool uuid-dev uuid dh-autoreconf -y
+sudo apt install ssh make vim build-essential git-core subversion libjansson-dev libsqlite3-dev autoconf automake libxml2-dev libncurses5-dev libtool uuid-dev uuid dh-autoreconf -y
 
-# Asterisk v13
+##############
+cd /usr/src/ #
+##############
 
-cd /usr/src/
-
+# Baixando o asterisk v13
 echo "Baixando o asterisk v13"
-wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
+sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
 
+# Descompactando o asterisk v13
 echo "Descompactando o asterisk v13"
-tar -zxvf asterisk-13-current.tar.gz
+sudo tar -zxvf asterisk-13-current.tar.gz
 
+# Apagando o arquivo compactado do asterisk v13
 echo "Apagando o arquivo compactado do asterisk v13"
-rm -rf asterisk-13-current.tar.gz
+sudo rm -rf asterisk-13-current.tar.gz
 
-echo "Instalando e configurando o asterisk v13"
+# Localizando o diretorio do asterisk v13
+echo "Localizando o diretorio do asterisk v13"
 asterisk=$(find . -name "asterisk-13.*")
-cd $asterisk
-./contrib/scripts/install_prereq install
-./bootstrap.sh
-./configure
-make && make install
-make samples
-make config
 
-# Codec G729
+##############
+cd $asterisk #
+##############
 
-cd /usr/src/
+# Configurando o asterisk v13
+echo "Configurando o asterisk v13"
+sudo ./contrib/scripts/install_prereq install
+sudo ./bootstrap.sh
+sudo ./configure
+sudo make && make install
+sudo make samples
+sudo make config
 
+##############
+cd /usr/src/ #
+##############
+
+# Baixando o codec g729
 echo "Baixando o codec g729"
-wget http://download-mirror.savannah.gnu.org/releases/linphone/plugins/sources/bcg729-1.0.0.tar.gz
+sudo wget http://download-mirror.savannah.gnu.org/releases/linphone/plugins/sources/bcg729-1.0.0.tar.gz
 
+# Descompactando o codec g729
 echo "Descompactando o codec g729"
-tar -xzf bcg729-1.0.0.tar.gz
+sudo tar -xzf bcg729-1.0.0.tar.gz
 
-echo "Instalando o codec g729"
-cd bcg729-1.0.0
+#################
+cd bcg729-1.0.0 #
+#################
 
-./configure --libdir=/lib
-make && make install
+# Configurando o codec g729
+echo "Configurando o codec g729"
+sudo ./configure --libdir=/lib
+sudo make && make install
 
-echo "Baixando o codec g72x"
-cd /usr/src
-wget http://asterisk.hosting.lv/src/asterisk-g72x-1.4.tar.bz2
+#############
+cd /usr/src #
+#############
 
+# Baixando o codec g72x
+sudo wget http://asterisk.hosting.lv/src/asterisk-g72x-1.4.tar.bz2
+
+# Descompactando o codec g729
 echo "Descompactando o codec g72x"
-tar -xjf asterisk-g72x-1.4.tar.bz2
+sudo tar -xjf asterisk-g72x-1.4.tar.bz2
 
-echo "Instalando o codec g72x"
-cd asterisk-g72x-1.4
-./autogen.sh
-./configure CFLAGS='-march=armv6' --with-asterisk130 --with-bcg729 --with-asterisk-includes=/usr/include
-make && make install
-chmod +x /usr/lib/asterisk/modules/codec_g729.so
+######################
+cd asterisk-g72x-1.4 #
+######################
 
+# Configurando o codec g72x
+echo "Configurando o codec g72x"
+sudo ./autogen.sh
+sudo ./configure CFLAGS='-march=armv6' --with-asterisk130 --with-bcg729 --with-asterisk-includes=/usr/include
+sudo make && make install
+sudo chmod +x /usr/lib/asterisk/modules/codec_g729.so
+
+# Apagando os arquivos compactados dos codecs
 echo "Apagando os arquivos compactados dos codecs"
 sudo rm -rf bcg729-1.0.0.tar.gz
 sudo rm -rf asterisk-g72x-1.4.tar.bz2
 
+# Iniciando o serviço do asterisk e verificando o status
 echo "Iniciando o serviço do asterisk"
-/etc/init.d/asterisk start
+sudo /etc/init.d/asterisk start
+sudo /etc/init.d/asterisk status
 
-echo "Testando o serviço do asterisk"
-/etc/init.d/asterisk status
+# Realizando backup dos arquivos sip.conf e extensions.conf do asterisk
+echo "Realizando backup dos arquivos sip.conf e extension.conf do asterisk"
+sudo mv /etc/asterisk/sip.conf /etc/asterisk/sip.conf.bkp
+sudo mv /etc/asterisk/extensions.conf /etc/asterisk/extensions.conf.bkp
 
-echo "Renomeando sip.conf e extension.conf padrão do asterisk"
-mv /etc/asterisk/sip.conf /etc/asterisk/sip.conf.bkp
-mv /etc/asterisk/extensions.conf /etc/asterisk/extensions.conf.bkp
+##############################################
+cd /usr/src/gateway-e1-raspberry-asteriskv13 #
+##############################################
 
-echo "Enviando arquivos para suas determinadas pastas"
-mkdir -p /etc/asterisk/krolik
-cd /usr/src/gateway-e1-raspberry-asteriskv13
-mv ./sip-peers.conf /etc/asterisk/krolik
-mv ./sip-trunk.conf /etc/asterisk/krolik
-mv ./sip.conf /etc/asterisk/sip.conf
-mv ./extensions.conf /etc/asterisk/extensions.conf
+# Movendo os arquivos configurados do sip.conf e extensions.conf
+echo "Movendo os arquivos configurados do sip.conf e extensions.conf"
+sudo mkdir -p /etc/asterisk/krolik
+sudo mv ./sip-peers.conf /etc/asterisk/krolik
+sudo mv ./sip-trunk.conf /etc/asterisk/krolik
+sudo mv ./sip.conf /etc/asterisk/sip.conf
+sudo mv ./extensions.conf /etc/asterisk/extensions.conf
 
+# Aplicando as configurações realizadas
 echo "Aplicando as configurações realizadas"
 sudo asterisk -rx 'sip reload'
 sudo asterisk -rx 'dialplan reload'
 
+# Verificando peers conectados
 echo "Verificando peers conectados"
 sudo asterisk -rx 'sip show peers'
